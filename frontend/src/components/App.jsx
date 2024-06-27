83150bed8e60ae9342edba7c713c5c6a97fd7381
Илья Копендох <ilakopendoh@MacBook-Air-Ila.local>
2024-06-27 20:55:38 +0300

    dasda

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -3,40 +3,55 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import MainPage from "./react-router/routes/MainPage";
 import Login from "./react-router/routes/Login";
 import SignUp from "./react-router/routes/SignUp";
-import { Provider } from "react-redux";
+import { Provider as ReduxProvider } from "react-redux";
 import store from "./react-router/redux/loginStore";
-import Testi18n from '../components/react-router/redux/Components/Testi18n'
+import Testi18n from "../components/react-router/redux/Components/Testi18n";
 import { errorOnRequest } from "../toast/notify";
-import leoProfanity from 'leo-profanity';
+import leoProfanity from "leo-profanity";
+import { Provider as RollbarProvider, ErrorBoundary } from "@rollbar/react"; // Provider imports 'rollbar'
+import rollbarConfig from "./rollbarConfig/RollBarConfig";
+
+
+
 
 const App = () => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
 
   useEffect(() => {
-    leoProfanity.loadDictionary('en')
-    leoProfanity.loadDictionary('ru')
+    leoProfanity.loadDictionary("en");
+    leoProfanity.loadDictionary("ru");
     const token = localStorage.getItem("token");
     setIsAuthenticated(!!token);
-    if(!navigator.onLine) {
-      errorOnRequest()
+    if (!navigator.onLine) {
+      errorOnRequest();
     }
   }, []);
-  
+
   return (
-    <Provider store={store}>
-      {!navigator.onLine ? errorOnRequest() : <BrowserRouter>
-        <Routes>
-          <Route
-            path="/"
-            element={isAuthenticated ? <MainPage /> : <Navigate to="/login" />}
-          />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<SignUp/>} />
-          <Route path="/test" element={<Testi18n/>} />
-          <Route path="*" element={<div> 404 (not found)</div>} />
-        </Routes>
-      </BrowserRouter> }
-    </Provider>
+    <RollbarProvider config={rollbarConfig}>
+      <ErrorBoundary>
+        <ReduxProvider store={store}>
+          {!navigator.onLine ? (
+            errorOnRequest()
+          ) : (
+            <BrowserRouter>
+              <Routes>
+                <Route
+                  path="/"
+                  element={
+                    isAuthenticated ? <MainPage /> : <Navigate to="/login" />
+                  }
+                />
+                <Route path="/login" element={<Login />} />
+                <Route path="/signup" element={<SignUp />} />
+                <Route path="/test" element={<Testi18n />} />
+                <Route path="*" element={<div> 404 (not found)</div>} />
+              </Routes>
+            </BrowserRouter>
+          )}
+        </ReduxProvider>
+      </ErrorBoundary>
+    </RollbarProvider>
   );
 };
 
---
+++ frontend/src/components/rollbarConfig/RollBarConfig.js
@@ -0,0 +1,6 @@
+const rollbarConfig = {
+    accessToken: "665363cda3654e609776e4119ae437e4",
+    environment: "testenv",
+  };
+  
+  export default rollbarConfig
