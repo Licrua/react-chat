0187b54d7327f1e6315f46a967d36c99fd6c7486
Илья Копендох <ilakopendoh@MacBook-Air-Ila.local>
2024-06-30 12:01:43 +0300

    dada

--- frontend/src/components/react-router/redux/Components/Testi18n.jsx
+++ frontend/src/components/react-router/redux/Components/Testi18n.jsx
@@ -1,21 +1,21 @@
-import { Container, Navbar, FormControl } from 'react-bootstrap';
-import { useEffect, useRef } from 'react';
+// import { Container, Navbar, FormControl, } from 'react-bootstrap';
+// import { useEffect, useRef } from 'react';
 import leoProfanity from 'leo-profanity';
-import Rebenok from './Rebenok';
-
+import { Dropdown, Button, ButtonGroup } from 'react-bootstrap';
+// import Rebenok from './Rebenok';
 const Component = () => {
-  const ref = useRef();
-  const ref2 = useRef();
-  const ref3 = useRef();
-  useEffect(() => {
-    ref3.current.focus();
-  });
-  const onFocus = () => {
-    ref.current.focus();
-  };
-  const onFocus2 = () => {
-    ref2.current.focus();
-  };
+  // const ref = useRef();
+  // const ref2 = useRef();
+  // const ref3 = useRef();
+  // useEffect(() => {
+  //   ref3.current.focus();
+  // });
+  // const onFocus = () => {
+  //   ref.current.focus();
+  // };
+  // const onFocus2 = () => {
+  //   ref2.current.focus();
+  // };
   const text = 'shit fuck moron fucker pussy dick';
   leoProfanity.loadDictionary('ru');
   leoProfanity.loadDictionary('en');
@@ -30,23 +30,33 @@ const Component = () => {
   checker();
 
   return (
-    <Navbar>
-      <Container>
-        <Rebenok ref={ref} ref2={ref2} />
-        <FormControl ref={ref3} type="email" placeholder="name@example.com" />
-        <button aria-label="focus" type="button" onClick={onFocus}>
-          focus
-        </button>
-        <button aria-label="focus2" type="button" onClick={onFocus2}>
-          focus2
-        </button>
-        <Navbar.Collapse className="justify-content-end">
-          <Navbar.Text className="justify-content-start">
-            Signed in as: <a href="#login">Mark Otto</a>
-          </Navbar.Text>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+    // <Navbar>
+    //   <Container>
+    //     <Rebenok ref={ref} ref2={ref2} />
+    //     <FormControl ref={ref3} type="email" placeholder="name@example.com" />
+    //     <button aria-label="focus" type="button" onClick={onFocus}>
+    //       focus
+    //     </button>
+    //     <button aria-label="focus2" type="button" onClick={onFocus2}>
+    //       focus2
+    //     </button>
+    //     <Navbar.Collapse className="justify-content-end">
+    //       <Navbar.Text className="justify-content-start">
+    //         Signed in as: <a href="#login">Mark Otto</a>
+    //       </Navbar.Text>
+    //     </Navbar.Collapse>
+    //   </Container>
+    // </Navbar>
+    <Dropdown as={ButtonGroup}>
+      <Button variant="secondary">Split Button</Button>
+
+      <Dropdown.Toggle split variant="secondary" id="dropdown-split-basic" />
+
+      <Dropdown.Menu>
+        <Dropdown.Item href="#/action-1">Удалить</Dropdown.Item>
+        <Dropdown.Item href="#/action-2">Переименовать</Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
   );
 };
 
